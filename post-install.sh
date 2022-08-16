c0b36923c630a3ccc80979cc1af9932f3371063f
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-08-16 17:50:18 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,58 +1,61 @@
-echo Change Password For root User
+echo Change Password For Kali User
 passwd
-apt-get update -y && apt-get upgrade -y
-apt install lynis -y
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install lynis -y
 echo Run Lynis & Output Report
-lynis audit system > ./lynis_log.txt
+sudo lynis audit system > ./lynis_log.txt
 echo Output open ports to log file
-netstat -tulpn > ./open_ports_log.txt
-apt install rkhunter -y
+sudo netstat -tulpn > ./open_ports_log.txt
+sudo apt install rkhunter -y
 echo Check For Rootkits Using RKHunter
-rkhunter -c
-apt install windows-binaries -y 
-wget https://download.sysinternals.com/files/SysinternalsSuite.zip /opt
-wget https://github.com/carlospolop/PEASS-ng/releases/download/20220814/linpeas.sh /opt/privesc/linux/linpeas.sh
-wget https://github.com/carlospolop/PEASS-ng/releases/download/20220814/winPEAS.bat /opt/privesc/windows/linpeas.bat
-git clone https://github.com/rebootuser/LinEnum.git /opt/privesc/linux/linenum
-git clone https://github.com/bitsadmin/wesng.git /opt/privesc/windows/exploit-suggester
-git clone https://github.com/nccgroup/ScoutSuite.git /opt/auditing/cloud/scoutsuite
-apt install python3-impacket -y
-apt install git -y
-apt install netcat-traditional -y
-apt install powershell  -y
-apt install shellter -y
-apt install netcat -y
-apt install tilix -y
-apt install iputils-ping -y
-apt install dirbuster -y
-apt install dirb -y
-apt install nano -y
-apt install nikto -y
-apt install net-tools -y
-apt install exploitdb -y
-apt install novnc -y
-apt install tcpdump -y
-apt install msfpc -y
-apt install smbclient -y
-apt install enum4linux -y
-apt install default-mysql-client -y
-gem install highline -y
-apt install snapd -y
-apt install prips -y
-apt install dirsearch -y
-apt install pip -y
-apt install rdesktop -y
-apt install seclists -y
-apt install dnsrecon -y
-apt install jython -y
-apt install sqlitebrowser -y
-apt install hashid -y
-apt install spray -y
-apt install poshc2 -y
-apt install bloodhound -y
-git clone https://github.com/sc0tfree/updog.git /opt
-gem install evil-winrm
-apt-get update -y
-apt autoremove
-updatedb
-service postgresql start
+sudo rkhunter -c
+sudo apt install windows-binaries -y 
+sudo wget https://download.sysinternals.com/files/SysinternalsSuite.zip /opt
+sudo wget https://github.com/carlospolop/PEASS-ng/releases/download/20220814/linpeas.sh /opt/privesc/linux/linpeas.sh
+sudo wget https://github.com/carlospolop/PEASS-ng/releases/download/20220814/winPEAS.bat /opt/privesc/windows/linpeas.bat
+sudo git clone https://github.com/paulkwalton/scripts.git /opt/scripts/
+sudo git clone https://github.com/rebootuser/LinEnum.git /opt/privesc/linux/linenum
+sudo git clone https://github.com/bitsadmin/wesng.git /opt/privesc/windows/exploit-suggester
+sudo git clone https://github.com/nccgroup/ScoutSuite.git /opt/auditing/cloud/scoutsuite
+sudo apt install python3-impacket -y
+sudo apt install git -y
+sudo apt install netcat-traditional -y
+sudo apt install powershell  -y
+sudo apt install shellter -y
+sudo apt install netcat -y
+sudo apt install tilix -y
+sudo apt install iputils-ping -y
+sudo apt install dirbuster -y
+sudo apt install dirb -y
+sudo apt install nano -y
+sudo apt install nikto -y
+sudo apt install net-tools -y
+sudo apt install exploitdb -y
+sudo apt install novnc -y
+sudo apt install tcpdump -y
+sudo apt install msfpc -y
+sudo apt install smbclient -y
+sudo apt install enum4linux -y
+sudo apt install default-mysql-client -y
+sudo gem install highline -y
+sudo apt install snapd -y
+sudo apt install prips -y
+sudo apt install dirsearch -y
+sudo apt install pip -y
+sudo apt install rdesktop -y
+sudo apt install seclists -y
+sudo apt install dnsrecon -y
+sudo apt install jython -y
+sudo apt install sqlitebrowser -y
+sudo apt install hashid -y
+sudo apt install spray -y
+sudo apt install poshc2 -y
+sudo apt install bloodhound -y
+sudo git clone https://github.com/sc0tfree/updog.git /opt
+sudo gem install evil-winrm
+sudo apt-get update -y
+sudo apt autoremove
+sudo updatedb
+sudo service postgresql start
+echo Change Password For root User
+sudo passwd
