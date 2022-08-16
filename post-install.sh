58d0281a1dfead0219918dea7934e2e2cf8a93da
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-08-16 21:07:39 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,12 +2,8 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install rkhunter -y
 echo Check For Rootkits Using RKHunter
 sudo rkhunter -c
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https -y
+sudo apt install default-jdk -y
+sudo apt install netbeans -y
 sudo apt update -y
 sudo apt install code -y
 sudo apt install windows-binaries -y 
@@ -71,4 +67,5 @@ sudo mkdir /etc/ssh/old_keys
 sudo mv /etc/ssh/ssh_host_* /etc/ssh/old_keys
 sudo dpkg-reconfigure openssh-server
 sudo macchanger -A eth0
+sudo ./Apache-NetBeans-12.0-bin-linux-x64.sh
 echo NOW CHANGE PASSWORDS
