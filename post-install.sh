f5268a7ae25def1c352651886754df8a1d52c0a4
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-08-16 14:54:25 +0100

    Create post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,35 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+    sudo apt install git -y
+    sudo apt install netcat-traditional -y
+    sudo apt install powershell  -y
+    apt install shellter -y
+    apt install netcat -y
+    apt install tilix -y
+    apt install iputils-ping -y
+    apt install dirbuster -y
+    apt install dirb -y
+    apt install nano -y
+    apt install nikto -y
+    apt install net-tools -y
+    apt install exploitdb -y
+    apt install novnc -y
+    apt install tcpdump -y
+    apt install msfpc -y
+    apt install smbclient -y
+    apt install enum4linux -y
+    apt install default-mysql-client -y
+    gem install highline -y
+    apt install snapd -y
+    apt install prips -y
+    apt install dirsearch -y
+    apt install pip -y
+    apt install rdesktop -y
+    apt install seclists -y
+    apt install dnsrecon -y
+    apt install jython -y
+    apt install sqlitebrowser -y
+    apt install hashid -y
+    gem install evil-winrm
+    sudo apt-get update -y
+    updatedb
+    service postgresql start
