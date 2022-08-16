e9c7755e836c6577bea28706abd14db805793874
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-08-16 19:25:15 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,14 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install rkhunter -y
 echo Check For Rootkits Using RKHunter
 sudo rkhunter -c
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update -y
+sudo apt install code -y
 sudo apt install windows-binaries -y 
 sudo wget https://download.sysinternals.com/files/SysinternalsSuite.zip /opt
 sudo wget https://github.com/carlospolop/PEASS-ng/releases/download/20220814/linpeas.sh /opt/privesc/linux/linpeas.sh
