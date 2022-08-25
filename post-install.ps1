0288043cbbbe47590d33ac8adcdfad4f290e56c8
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-08-25 11:48:03 +0100

    Update post-install.ps1

--- post-install.ps1
+++ post-install.ps1
@@ -1,11 +1,11 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install adobereader
-choco install googlechrome
-choco install firefox
-choco install jre8
-choco install notepadplusplus.install
-choco install 7zip.install
-choco install python
-choco install sysinternals
-choco install putty.install
-choco install vscode
+choco install adobereader -y
+choco install googlechrome -y
+choco install firefox -y
+choco install jre8 -y
+choco install notepadplusplus.install -y
+choco install 7zip.install -y
+choco install python -y
+choco install sysinternals -y
+choco install putty.install -y
+choco install vscode -y
