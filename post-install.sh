7d582a024fb4ffed655ea7fb61c638d76bf2fe37
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-08-31 07:39:57 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -89,6 +89,7 @@ sudo apt install bloodhound -y
 sudo apt install responder -y
 sudo apt install yersinia -y
 sudo apt install spiderfoot -y
+sudo apt install amass-common -y
 dpkg --add-architecture i386 && apt-get update -y && apt-get install wine32:i386 -y
 sudo gem install evil-winrm
 sudo apt-get update -y
