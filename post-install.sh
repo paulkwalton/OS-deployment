aafca4a622f33226bb69ffd97c5ab026571c5dcd
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-10-07 17:40:06 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -97,6 +97,9 @@ sudo pip install mitm6
 sudo pip install exrex
 sudo exrex "((W|w)inter|(S|s)ummer|(F|f)all|(A|a)utumn|(S|s)pring)20(16|17|18|19|20|21|22)" > /opt/custom-passwords.txt
 sudo exrex "((W|w)elcome)(1|2|3|4|5|6|7|8|9)" >> /opt/custom-passwords.txt
+sudo exrex "((W|w)elcome)(1|12|123|1234|12345|123456|1234567|12345678|123456789)" >> /opt/custom-passwords.txt
+sudo exrex "((W|w)elcome@)(1|2|3|4|5|6|7|8|9)" >> /opt/custom-passwords.txt
+sudo exrex "((W|w)elcome@)(1|12|123|1234|12345|123456|1234567|12345678|123456789)" >> /opt/custom-passwords.txt
 sudo exrex "((P|p)assword)(1|2|3|4|5|6|7|8|9|0)" >> /opt/custom-passwords.txt
 sudo exrex "((P|p)assw0rd)(1|2|3|4|5|6|7|8|9|0)" >> /opt/custom-passwords.txt
 sudo exrex "((J|j)anuary|(F|f)ebruary|(M|m)arch|(A|a)pril|(M|m)ay|(J|j)une|(J|j)uly|(A|a)ugust|(S|s)eptember|(O|o)ctober|(N|n)ovember|(D|d)ecember)20(16|17|18|19|20|21|22)" >> /opt/custom-passwords.txt
