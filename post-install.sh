ca5cd2469b204a8be7c224db0115d38e0fc7518c
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2022-10-12 20:43:10 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -30,6 +30,7 @@ sudo git clone https://github.com/sc0tfree/updog.git /opt/filehosting
 sudo git clone https://github.com/Flangvik/SharpCollection.git /opt/sharpcollection/
 sudo git clone https://github.com/google/tsunami-security-scanner.git /opt/google-tsunami/
 sudo git clone https://github.com/phra/PEzor.git /opt/PEzor
+sudo git clone https://github.com/FortyNorthSecurity/RandomScripts.git /opt/shellcode-formatter
 sudo mkdir -p /adtools/sharphound
 sudo wget -O /opt/adtools/sharphound/sharphound.ps1 https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.ps1
 sudo wget -O /opt/adtools/sharphound.exe https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.exe
