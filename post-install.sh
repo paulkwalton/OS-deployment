6c578ef3fa60e8b141dc0d0c3b58f464b3f3e27c
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2023-01-05 13:37:47 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -42,7 +42,6 @@ sudo git clone https://github.com/Flangvik/SharpCollection.git /opt/sharpcollect
 sudo git clone https://github.com/phra/PEzor.git /opt/PEzor
 sudo git clone https://github.com/FortyNorthSecurity/RandomScripts.git /opt/shellcode-formatter
 sudo git clone https://github.com/hashcat/kwprocessor.git /opt/kwprocessor
-sudo git clone https://github.com/leoloobeek/LAPSToolkit.git /opt/LAPSToolkit
 sudo git clone https://github.com/dirkjanm/PrivExchange.git /opt/PrivExchange
 sudo git clone https://github.com/mdsecactivebreach/Chameleon.git /opt/chameleon
 sudo git clone https://github.com/Arvanaghi/SessionGopher.git /opt/privesc/windows/sessiongopher
